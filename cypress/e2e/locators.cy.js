b465ee9d98bb437298f3b19ddd5d011b32d05c50
Enes Aybars Aydın <aybarsaybars@gmail.com>
2023-05-07 16:35:31 +0300

    add: last changes

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -10,7 +10,19 @@ describe('Find or Get Elements by Using Different Locators', () => {
     })
 
     it('Check different locator strategies',  () => {  
+        // By CSS locator 
+        cy.get("input[name='username']").type("CydeoStudent");
+        // Every statement creates an object to be interacted, and next command makes operation created at the previous statement 
         
+
+        // attribute name and value
+        cy.get("[type='text']").clear();  // clear what is typed
+
+        cy.get("input").each((item, index, list) => {
+            // assert the length of the list is 2
+            expect(list).to.have.length(2);
+            expect(item).to.have.attr("type");
+        })
 
     })
 
