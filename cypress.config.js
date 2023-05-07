e23d131e1d15fea69dc6a392c658a7ceed47a872
Enes Aybars Aydın <aybarsaybars@gmail.com>
2023-05-08 00:15:43 +0300

    add: config file

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'https://practice.cydeo.com/',
     video: false,
     retries: 0, // if test case fails it will try to run again, with make this 0 we cancel to second attempt basically ignore retry
+    defaultCommandTimeout: 5000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
