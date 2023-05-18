74f955ff73d378100eb7db14979fa0065e13d98d
Enes Aybars Aydın <aybarsaybars@gmail.com>
2023-05-18 19:31:37 +0300

    new lines added

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,9 @@ module.exports = defineConfig({
     video: false,
     retries: 0, // if test case fails it will try to run again, with make this 0 we cancel to second attempt basically ignore retry
     defaultCommandTimeout: 5000,
+    screenshotOnRunFailure: true,
+    chromeWebSecurity: false,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -1,5 +1,6 @@
 ///  <reference types="cypress" />
 
+const { curry } = require('cypress/types/lodash');
 const { check } = require('prettier');
 
 describe('Input Forms Tests', () => {
@@ -8,7 +9,7 @@ describe('Input Forms Tests', () => {
     cy.visit('/registration_form');
   });
 
-  xit('Check different input box fields and verify', () => {
+  it('Check different input box fields and verify', () => {
     // fill the form for username and other info
     cy.get('input[name="firstname"]').type('Mike');
     cy.get('input[name="lastname"]').type('Tyson');
@@ -32,8 +33,6 @@ describe('Input Forms Tests', () => {
     cy.get('input[name="birthday"]').type('01/01/1999');
   });
 
-  xit('Check different input box fields and verify', () => {});
-
   it('Check different radio box actions', () => {
     cy.get('.radio')
       .find('[type=radio]')
@@ -51,9 +50,10 @@ describe('Input Forms Tests', () => {
          *              it basically asserts whether it is checked
          */
 
-        // get all radio buttons, select the second one, verify that it is checked
+        // get all radio buttons, select the second one, verify that it is checked and confirmation label is visible
         cy.wrap(radio).eq(1).check().should('be.checked');
-        cy.get('data-bv-icon-for="gender"').should('be.visible');
+        cy.get('data-bv-icon-for="gender"').should('be.visible');  // <- Check this out!
+        // !!! NO SUCH ELEMENT BCZ PAGE IS NOT AS ORIGINAL FIND OUT WHY !!!
 
         // third radio button is NOT checked
         cy.wrap(radio).eq(2).should('not.be.checked');
@@ -73,9 +73,57 @@ describe('Input Forms Tests', () => {
     });
   });
 
-  it('Check different checkbox actions', () => {});
+  it('Check different input box fields and verify', () => {
 
-  xit('Check different checkbox actions', () => {});
+    // get all input boxes, type some information 
+
+    cy.get("input[name='firstname']").type('Enes Aybars');
+    cy.get("input[name='lastname']").type('Aydin');
+    cy.get("input[name='username']").type('iBars');
+    cy.get("input[name='password']").type('ronaldo7');
+
+    let email = `aybars@gmail.com`
+    cy.get("input[name='email']").type(email);
+
+    let phoneNummer = `05${Math.floor(100000000 + Math.random() * 900000000)}`;
+    cy.get("input[name='phone']").type(phoneNummer);
+    cy.get("input[name='birthday']").type('10/02/1998');
+
+  });
+
+  it('Check selection of a single choice from a dropdown', () => {
+
+    // select one element
+    cy.get("select[name='job_title']").select("SDET");
+    
+    // assert that dropdown has correct text after selecting
+    cy.get("select[name='job_title']").contains("SDET");
+
+  });
+
+  it.only('Check selection of all select dropdowns option', () => {
+  
+    // we will provide our test data through fixtures folder as JSON object, then use that data to verify select values
+    cy.fixture("departments").then((eachDepartment) => {
+     
+      // get all options in the menu, iterate through these options one by one
+      cy.get("select[name='department'] > option").each((option, index) => {
+        
+        // get each option text
+        const optionText = option.text();
+        cy.log(optionText);
+        cy.log(index);
+        cy.log(eachDepartment[index]);
+
+        cy.get("select[name='department']").select()(optionText)
+        .should('have.value', option.val())        
+        .contains(eachDepartment[index]);
+
+      });
+    });
+    
+  });
+
 
   // to open cypress console code: npm run cypress-cli
 });
